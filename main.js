852e545771caf0843ed343ad49904e179105f61b
gabrieldandrea <gabbdan@gmail.com>
2020-12-28 21:36:19 -0500

    added main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client()
+
+client.once("ready", () => {
+    console.log("Russian Spy is online!")
+});
+
+client.login("NzkzMjcxNDUwNTU0NTk3NDE2.X-p1nA.1-oWfW_oAxCgn-ZJb74DaPuyCX0");
