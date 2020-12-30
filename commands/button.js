30b13f45f3471b87c428610e2e30f0d484abc469
gabrieldandrea <gabbdan@gmail.com>
2020-12-30 17:21:21 -0500

    changed timers from 10 seconds to 30

--- commands/button.js
+++ commands/button.js
@@ -3,15 +3,15 @@ module.exports = {
     description: "Button defusing kit",
     execute(message, args) {
         const filter = m => m.author.id === message.author.id;
-        message.reply("Is the button blue and does it say 'Abort'? These messages will expire in 10 seconds").then(r => r.delete({ timeout: 10000 }));
-        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+        message.reply("Is the button blue and does it say 'Abort'? These messages will expire in 30 seconds").then(r => r.delete({ timeout: 30000 }));
+        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
             let input = collected.first().content.toLowerCase();
 
             if (input === "cancel") {
                 return message.reply("Canceled");
             } else if (input === "yes") {
-                message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 10000 }));
-                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 30000 }));
+                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                     let input = collected.first().content.toLowerCase();
 
                     return message.channel.send(stripColour(input));
@@ -19,8 +19,8 @@ module.exports = {
                     message.reply("Time has expired").then(r => r.delete({ timeout: 5000 }));
                 });
             } else {
-                message.reply("Is there is more than 1 battery on the bomb and the button says 'Detonate'?").then(r => r.delete({ timeout: 10000 }));
-                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                message.reply("Is there is more than 1 battery on the bomb and the button says 'Detonate'?").then(r => r.delete({ timeout: 30000 }));
+                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                     let input = collected.first().content.toLowerCase();
 
                     if (input === "cancel") {
@@ -28,15 +28,15 @@ module.exports = {
                     } else if (input === "yes") {
                         return message.channel.send("Press and immediately release the button");
                     } else {
-                        message.reply("Is the button white and there is a lit indicator labelled 'CAR'?").then(r => r.delete({ timeout: 10000 }));
-                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                        message.reply("Is the button white and there is a lit indicator labelled 'CAR'?").then(r => r.delete({ timeout: 30000 }));
+                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                             let input = collected.first().content.toLowerCase();
 
                             if (input === "cancel") {
                                 return message.reply("Canceled");
                             } else if (input === "yes") {
-                                message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 10000 }));
-                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 30000 }));
+                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                     let input = collected.first().content.toLowerCase();
 
                                     return message.channel.send(stripColour(input));
@@ -44,8 +44,8 @@ module.exports = {
                                     message.reply("Time has expired").then(r => r.delete({ timeout: 5000 }));
                                 });
                             } else {
-                                message.reply("Are there are more than 2 batteries on the bomb and a lit indicator labelled 'FRK'?").then(r => r.delete({ timeout: 10000 }));
-                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                message.reply("Are there are more than 2 batteries on the bomb and a lit indicator labelled 'FRK'?").then(r => r.delete({ timeout: 30000 }));
+                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                     let input = collected.first().content.toLowerCase();
 
                                     if (input === "cancel") {
@@ -53,15 +53,15 @@ module.exports = {
                                     } else if (input === "yes") {
                                         return message.channel.send("Press and immediately release the button");
                                     } else {
-                                        message.reply("Is the button yellow?").then(r => r.delete({ timeout: 10000 }));
-                                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                        message.reply("Is the button yellow?").then(r => r.delete({ timeout: 30000 }));
+                                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                             let input = collected.first().content.toLowerCase();
 
                                             if (input === "cancel") {
                                                 return message.reply("Canceled");
                                             } else if (input === "yes") {
-                                                message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 10000 }));
-                                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                                message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 30000 }));
+                                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                                     let input = collected.first().content.toLowerCase();
 
                                                     return message.channel.send(stripColour(input));
@@ -69,8 +69,8 @@ module.exports = {
                                                     message.reply("Time has expired").then(r => r.delete({ timeout: 5000 }));
                                                 });
                                             } else {
-                                                message.reply("Is the button red and does it say 'Hold'?").then(r => r.delete({ timeout: 10000 }));
-                                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                                message.reply("Is the button red and does it say 'Hold'?").then(r => r.delete({ timeout: 30000 }));
+                                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                                     let input = collected.first().content.toLowerCase();
 
                                                     if (input === "cancel") {
@@ -78,8 +78,8 @@ module.exports = {
                                                     } else if (input === "yes") {
                                                         return message.channel.send("Press and immediately release the button");
                                                     } else {
-                                                        message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 10000 }));
-                                                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                                        message.reply("Hold the button, what is the colour of the strip?").then(r => r.delete({ timeout: 30000 }));
+                                                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                                             let input = collected.first().content.toLowerCase();
         
                                                             return message.channel.send(stripColour(input));
--- commands/wires.js
+++ commands/wires.js
@@ -3,15 +3,15 @@ module.exports = {
     description: "Horizontal wires defusing kit",
     execute(message, args) {
         const filter = m => m.author.id === message.author.id;
-        message.reply("Enter the number of wires, these messages will expire in 10 seconds").then(r => r.delete({ timeout: 10000 }));
-        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+        message.reply("Enter the number of wires, these messages will expire in 30 seconds").then(r => r.delete({ timeout: 30000 }));
+        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
             let input = collected.first().content.toLowerCase();
 
             if (input === "cancel") {
                 return message.reply("Canceled");
             } else if (input === "3") {
-                message.reply("Are there any red wires?").then(r => r.delete({ timeout: 10000 }));
-                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                message.reply("Are there any red wires?").then(r => r.delete({ timeout: 30000 }));
+                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                     let input = collected.first().content.toLowerCase();
 
                     if (input === "cancel") {
@@ -19,8 +19,8 @@ module.exports = {
                     } else if (input === "no") {
                         return message.reply("Cut the last wire");
                     } else {
-                        message.reply("Is the last wire white?").then(r => r.delete({ timeout: 10000 }));
-                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                        message.reply("Is the last wire white?").then(r => r.delete({ timeout: 30000 }));
+                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                             let input = collected.first().content.toLowerCase();
 
                             if (input === "cancel") {
@@ -28,8 +28,8 @@ module.exports = {
                             } else if (input === "yes") {
                                 return message.reply("Cut the last wire");
                             } else {
-                                message.reply("Are there more than one blue wire?").then(r => r.delete({ timeout: 10000 }));
-                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                message.reply("Are there more than one blue wire?").then(r => r.delete({ timeout: 30000 }));
+                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                     let input = collected.first().content.toLowerCase();
 
                                     if (input === "cancel") {
@@ -54,8 +54,8 @@ module.exports = {
                     message.reply("Time has expired").then(r => r.delete({ timeout: 5000 }));
                 });
             } else if (input === "4") {
-                message.reply("Are there more than one red wire and is the last digit of the serial number odd?").then(r => r.delete({ timeout: 10000 }));
-                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                message.reply("Are there more than one red wire and is the last digit of the serial number odd?").then(r => r.delete({ timeout: 30000 }));
+                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                     let input = collected.first().content.toLowerCase();
 
                     if (input === "cancel") {
@@ -63,8 +63,8 @@ module.exports = {
                     } else if (input === "yes") {
                         return message.reply("Cut the last red wire");
                     } else {
-                        message.reply("Is the last wire yellow and are there no red wires?").then(r => r.delete({ timeout: 10000 }));
-                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                        message.reply("Is the last wire yellow and are there no red wires?").then(r => r.delete({ timeout: 30000 }));
+                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                             let input = collected.first().content.toLowerCase();
 
                             if (input === "cancel") {
@@ -72,8 +72,8 @@ module.exports = {
                             } else if (input === "yes") {
                                 return message.reply("Cut the first wire");
                             } else {
-                                message.reply("Is there exactly one blue wire?").then(r => r.delete({ timeout: 10000 }));
-                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                message.reply("Is there exactly one blue wire?").then(r => r.delete({ timeout: 30000 }));
+                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                     let input = collected.first().content.toLowerCase();
 
                                     if (input === "cancel") {
@@ -81,8 +81,8 @@ module.exports = {
                                     } else if (input === "yes") {
                                         return message.reply("Cut the first wire");
                                     } else {
-                                        message.reply("Are there more than one yellow wire?").then(r => r.delete({ timeout: 10000 }));
-                                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                        message.reply("Are there more than one yellow wire?").then(r => r.delete({ timeout: 30000 }));
+                                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                             let input = collected.first().content.toLowerCase();
 
                                             if (input === "cancel") {
@@ -112,8 +112,8 @@ module.exports = {
                     message.reply("Time has expired").then(r => r.delete({ timeout: 5000 }));
                 });
             } else if (input === "5") {
-                message.reply("Is the last wire black and is the last digit of the serial number odd?").then(r => r.delete({ timeout: 10000 }));
-                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                message.reply("Is the last wire black and is the last digit of the serial number odd?").then(r => r.delete({ timeout: 30000 }));
+                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                     let input = collected.first().content.toLowerCase();
 
                     if (input === "cancel") {
@@ -121,8 +121,8 @@ module.exports = {
                     } else if (input === "yes") {
                         return message.reply("Cut the fourth wire");
                     } else {
-                        message.reply("Is there exactly one red wire and more than one yellow wire?").then(r => r.delete({ timeout: 10000 }));
-                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                        message.reply("Is there exactly one red wire and more than one yellow wire?").then(r => r.delete({ timeout: 30000 }));
+                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                             let input = collected.first().content.toLowerCase();
 
                             if (input === "cancel") {
@@ -130,8 +130,8 @@ module.exports = {
                             } else if (input === "yes") {
                                 return message.reply("Cut the first wire");
                             } else {
-                                message.reply("Are there no black wires?").then(r => r.delete({ timeout: 10000 }));
-                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                message.reply("Are there no black wires?").then(r => r.delete({ timeout: 30000 }));
+                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                     let input = collected.first().content.toLowerCase();
 
                                     if (input === "cancel") {
@@ -156,8 +156,8 @@ module.exports = {
                     message.reply("Time has expired").then(r => r.delete({ timeout: 5000 }));
                 });
             } else if (input === "6") {
-                message.reply("Are there no yellow wires and is the last digit of the serial number odd?").then(r => r.delete({ timeout: 10000 }));
-                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                message.reply("Are there no yellow wires and is the last digit of the serial number odd?").then(r => r.delete({ timeout: 30000 }));
+                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                     let input = collected.first().content.toLowerCase();
 
                     if (input === "cancel") {
@@ -165,8 +165,8 @@ module.exports = {
                     } else if (input === "yes") {
                         return message.reply("Cut the third wire");
                     } else {
-                        message.reply("Is there exactly one yellow wire and more than one white wire?").then(r => r.delete({ timeout: 10000 }));
-                        message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                        message.reply("Is there exactly one yellow wire and more than one white wire?").then(r => r.delete({ timeout: 30000 }));
+                        message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                             let input = collected.first().content.toLowerCase();
 
                             if (input === "cancel") {
@@ -174,8 +174,8 @@ module.exports = {
                             } else if (input === "yes") {
                                 return message.reply("Cut the fourth wire");
                             } else {
-                                message.reply("Are there any red wires?").then(r => r.delete({ timeout: 10000 }));
-                                message.channel.awaitMessages(filter, { max: 1, time: 10000 }).then(collected => {
+                                message.reply("Are there any red wires?").then(r => r.delete({ timeout: 30000 }));
+                                message.channel.awaitMessages(filter, { max: 1, time: 30000 }).then(collected => {
                                     let input = collected.first().content.toLowerCase();
 
                                     if (input === "cancel") {
