69cd16032981b265e9dbefc1fbe7561ec368fa50
gabrieldandrea <gabbdan@gmail.com>
2021-01-14 19:40:22 -0500

    removed ignored files

--- commands/oldserver.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "oldserver",
-    description: "this shows how the old server was",
-    execute(message, args) {
-        return message.channel.send("guys this is literally like our old server..gabe and nick or whoever doing project e..everyone did their own thing so why would I work hard to get money to buy something i didnt want in the first plae");
-    }
-}
